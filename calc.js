0a9af3719e82f776bcf1dd82ee35d347ffbfd886
= <=>
2020-09-18 22:53:07 -0300

    add and minus opertaions

--- calc.js
+++ calc.js
@@ -1,15 +1,18 @@
 var colors = require('colors');
 const {argv} = require('yargs')
- 
+const {sum,minus} = require('./operations/basic')
+
+
     // if ( argv.b != 0) {
     // console.log(' a/b = ', argv.a/argv.b)
     // } else {
     // console.log('Donkey XD'.red)
     // }
 
+    console.log(sum(argv.a, argv.b));
+    console.log(minus(argv.a, argv.b));
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
 
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-
-numbers.forEach(element => {
-    console.log(`${argv.a}x${element} =`,argv.a * element );
-});
+// numbers.forEach(element => {
+//     console.log(`${argv.a}x${element} =`,argv.a * element );
+// });
---
+++ operations/basic.js
@@ -0,0 +1,11 @@
+let sum = (a,b)=>{
+    return a+b;
+}
+let minus = (a,b)=>{
+return a-b
+}
+
+module.exports = {
+    sum,
+    minus
+}
